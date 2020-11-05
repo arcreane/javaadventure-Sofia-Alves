91cee11bcba5b654368486b693cb3cb9c6a5096c
SofiaAlves <alveslaura.sofia@gmail.com>
2020-11-05 10:05:37 +0100

    premier commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,15 @@ package com.company;
 
 public class Main {
 
+    public Main() {
+    }
+
     public static void main(String[] args) {
-	// write your code here
+
+        System.out.println("Bienvenue dans le dongeon");
+        System.out.println("Passe la porte");
+        System.out.println("Bienvenue dans le dongeon");
+
+
     }
 }
